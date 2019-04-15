44e55296859d5a4ca9bf924476b8c0b2d2d0c8b0
bogdan.neacsu <bogdan.neacsu@hotmail.com>
2019-04-15 17:27:51 +0300

    test get instance by meta data filtering

--- test-eureka-consumer/src/main/java/com/boneacsu/research/discovery/eureka/consumer/FeignClientApplication.java
+++ test-eureka-consumer/src/main/java/com/boneacsu/research/discovery/eureka/consumer/FeignClientApplication.java
@@ -1,5 +1,11 @@
 package com.boneacsu.research.discovery.eureka.consumer;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
+import com.netflix.discovery.shared.Applications;
+import com.netflix.eureka.EurekaServerContextHolder;
+import com.netflix.eureka.registry.PeerAwareInstanceRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +13,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -15,6 +25,9 @@ public class FeignClientApplication {
     @Autowired
     private GreetingClient greetingClient;
 
+    @Autowired
+    private EurekaClient discoveryClient;
+
     public static void main(String[] args) {
         SpringApplication.run(FeignClientApplication.class, args);
     }
@@ -22,6 +35,27 @@ public class FeignClientApplication {
     @RequestMapping("/get-greeting")
     public String greeting(Model model) {
         model.addAttribute("greeting", greetingClient.greeting());
+        return "greeting-view";
+    }
+
+    @RequestMapping("/get-byMeta")
+    public String greeting(
+            @RequestParam("key") String key,
+            @RequestParam("value") String value,
+            Model model) {
+
+        List<InstanceInfo> instances =
+                discoveryClient.getApplications()
+                        .getRegisteredApplications("spring-cloud-eureka-client")
+                        .getInstances();
+
+        List<String> instancesMatching = instances.stream()
+                .filter((i) ->
+                        i.getMetadata().containsKey(key) && i.getMetadata().get(key).equals(value))
+                .map((i) -> i.getInstanceId())
+                .collect(Collectors.toList());
+
+        model.addAttribute("greeting", "List of filtered apps : " + instancesMatching.toString());
         return "greeting-view";
     }
 }
